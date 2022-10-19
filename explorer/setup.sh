e04d2e827dc8744fef01360fb73eaaba7bb005d3
Mira <miranabila128@gmail.com>
2022-10-19 10:18:09 +0700

    del

--- explorer/setup.sh
+++
@@ -1,4 +0,0 @@
-export PETANI_MSPKEY=$(cd ../test-network/organizations/peerOrganizations/petani.coedinus.com/users/Admin@petani.coedinus.com/msp/keystore && ls *_sk) 
-
-
-sed -i  "s/PETANI_MSPKEY/$PETANI_MSPKEY/g" first-network.json 
