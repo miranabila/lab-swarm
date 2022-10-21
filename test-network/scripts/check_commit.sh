dcc805bbdc6273e36b1380e6e2f343d174d484f3
Mira <miranabila128@gmail.com>
2022-10-21 21:13:32 +0700

    del

--- test-network/scripts/check_commit.sh
+++
@@ -1,4 +0,0 @@
-
-
-echo "check commit readiness"
-peer lifecycle chaincode checkcommitreadiness --channelID $CHANNEL_NAME --name $CC_NAME --version 1 --sequence 1 --init-required --signature-policy "OR ('petaniMSP.peer','pengepulMSP.peer','distributorMSP.peer','retailerMSP.peer')" 
